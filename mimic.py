16f627372c406924ca46f81316cca81db9da2777
SONDOS <sondosissa18@gmail.com>
2020-08-21 12:28:23 -0500

    passed all tests

--- mimic.py
+++ mimic.py
@@ -20,7 +20,8 @@ You can try adding in line breaks around 70 columns so the output looks
 better.
 """
 
-__author__ = "???"
+__author__ = "sondos with help from Gabby and Shanqual"
+# https://www.programmersought.com/article/74581508841/
 
 
 import random
@@ -28,25 +29,19 @@ import sys
 
 
 def create_mimic_dict(filename):
-    """Returns a dict mapping each word to a list of words which follow it.
-    For example:
-        Input: "I am a software developer, and I don't care who knows"
-        Output:
-            {
-                "" : ["I"],
-                "I" : ["am", "don't"],
-                "am": ["a"],
-                "a": ["software"],
-                "software" : ["developer,"],
-                "developer," : ["and"],
-                "and" : ["I"],
-                "don't" : ["care"],
-                "care" : ["who"],
-                "who" : ["knows"]
-            }
-    """
-    # +++your code here+++
-    pass
+
+    mimic_dict = dict()
+    with open(filename) as f:
+        text = f.read()
+        words = text.split()
+        prev_word = ''
+        for word in words:
+            if prev_word in mimic_dict:
+                mimic_dict[prev_word].append(word)
+            else:
+                mimic_dict[prev_word] = [word]
+            prev_word = word
+    return mimic_dict
 
 
 def print_mimic_random(mimic_dict, num_words):
@@ -58,8 +53,13 @@ def print_mimic_random(mimic_dict, num_words):
         - Randomly select a new word from the next-list
         - Repeat this process num_words times
     """
-    # +++your code here+++
-    pass
+    start_word = ''
+    for ___ in range(num_words + 1):
+        next_word = mimic_dict.get(start_word)
+        print(start_word, end=' ')
+        if next_word is None:
+            next_word = mimic_dict['']
+        start_word = random.choice(next_word)
 
 
 def main(args):
